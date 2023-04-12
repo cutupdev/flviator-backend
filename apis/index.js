ed2e7a04edeb4ad9d082817b945e4ab0b57e5f8a
CatLover <takasi75926@gmail.com>
2023-04-12 14:58:17 +0300

    change post to get

--- apis/index.js
+++ apis/index.js
@@ -3,8 +3,8 @@ const History = require("./history");
 const API = (router) => {
     router.post('/myInfo', History.myInfo);
     router.post('/topHistory', History.topHistory);
-    router.post('/getTotalHistory', History.totalHistory);
-    router.post('/getTotalUsers', History.totalUsers);
+    router.get('/getTotalHistory', History.totalHistory);
+    router.get('/getTotalUsers', History.totalUsers);
 }
 
 module.exports = API;
