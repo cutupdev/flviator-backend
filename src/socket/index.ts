09a717393ab4696d148f9a8ca7d1c6a053ad497e
CatLover <takasi75926@gmail.com>
2023-12-14 07:34:29 +0900

    Hotfix: checking bot

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -551,12 +551,12 @@ export const initSocket = (io: Server) => {
         })
 
         setInterval(() => {
-            // if (GameState === NextGameState) {
-            // NextGameState = NextState;
-            const time = Date.now() - startTime;
-            io.emit('gameState', { currentNum, currentSecondNum, GameState, time });
-            // }
-            sendInfo();
+            if (GameState === NextGameState) {
+                NextGameState = NextState;
+                const time = Date.now() - startTime;
+                io.emit('gameState', { currentNum, currentSecondNum, GameState, time });
+                sendInfo();
+            }
         }, 100)
     });
 
