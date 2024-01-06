9c28c0e9013da3290d1767b0894586837210bdaa
CatLover <takasi75926@gmail.com>
2024-01-06 02:02:00 -0800

    Hotfix: add username

--- src/controllers/client.ts
+++ src/controllers/client.ts
@@ -61,8 +61,9 @@ export const GameLaunch = async (req: Request, res: Response) => {
             var _data = resData.data;
             console.log("_data", _data)
             if (!userData) {
+                _data = _data.data;
                 let balance = Number(_data.balance) || 0;
-                userData = await addUser(_data.userName, UserID, _data.currency, balance, _data.avatar, "", "admin", ipAddress)
+                userData = await addUser(_data.userName || "username", UserID, _data.currency || "INR", balance || 0, _data.avatar || "", "", "admin", ipAddress || "0.0.0.0")
             }
 
 
